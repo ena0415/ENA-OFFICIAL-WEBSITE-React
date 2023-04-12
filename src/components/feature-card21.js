626bbc7c39bf08843281a423dab2c327de8ddffc
ena0415 <129664127+ena0415@users.noreply.github.com>
2023-04-12 17:29:28 +0900

    Commit from TeleportHQ

--- src/components/feature-card21.js
+++ src/components/feature-card21.js
@@ -9,14 +9,16 @@ const FeatureCard21 = (props) => {
     <div className={`feature-card21-feature-card ${props.rootClassName} `}>
       <div className="feature-card21-container">
         <div className="feature-card21-container1">
-          <svg viewBox="0 0 1024 1024" className="feature-card21-icon">
-            <path
-              d="M512 128h256v170h-170v428q0 70-51 120t-121 50-120-50-50-120 50-121 120-51q42 0 86 24v-450z"
-              className=""
-            ></path>
-          </svg>
           <div className="feature-card21-container2">
-            <div className="feature-card21-container3">
+            <svg viewBox="0 0 1024 1024" className="feature-card21-icon">
+              <path
+                d="M512 128h256v170h-170v428q0 70-51 120t-121 50-120-50-50-120 50-121 120-51q42 0 86 24v-450z"
+                className=""
+              ></path>
+            </svg>
+          </div>
+          <div className="feature-card21-container3">
+            <div className="feature-card21-container4">
               <h3 className="feature-card21-text Heading3">{props.title}</h3>
               <span className="feature-card21-text1">{props.artist}</span>
               <span className="feature-card21-text2">{props.year}</span>
